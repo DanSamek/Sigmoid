b263be6892478367b0658cd175ee69c7c932e1fa
Daniel Samek <samekdaniel@email.cz>
2025-06-08 20:40:20 +0200

    better asp. windows
    bench 2092002

--- src/worker.hpp
+++ src/worker.hpp
@@ -58,7 +58,7 @@ namespace Sigmoid {
                     continue;
                 }
 
-                int16_t delta = 20;
+                int16_t delta = 10;
                 int16_t alpha = std::max(MIN_VALUE, (int16_t)(eval - delta));
                 int16_t beta = std::min(MAX_VALUE, (int16_t)(eval + delta));
 
@@ -66,11 +66,10 @@ namespace Sigmoid {
                     eval = negamax<ROOT>(depth, alpha, beta, root);
 
                     if (eval <= alpha && eval > -CHECKMATE_BOUND){
-                        // beta = (eval + beta) / 2; todo try, if pass.
                         alpha -= delta;
+                        beta = (eval + beta) / 2;
                     }
                     else if (eval >= beta && eval < CHECKMATE_BOUND){
-                        // alpha = (eval + alpha) / 2; todo try, if pass.
                         beta += delta;
                     }
                     else{
