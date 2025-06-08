d8021e1952797af4cb4c4ecb7b474eaf9d9e4fb9
Daniel Samek <samekdaniel@email.cz>
2025-06-08 12:59:43 +0200

    1 ply continuation history
    bench 2683832

--- src/history.hpp
+++ src/history.hpp
@@ -8,30 +8,39 @@
 #include "constants.hpp"
 
 namespace Sigmoid{
-    template<typename T, std::size_t... Dimensions>
+    template<typename T, T MaxValue, std::size_t... Dimensions>
     struct History;
 
-    template<typename T, std::size_t First, std::size_t... Rest>
-    struct History<T, First, Rest...> {
-        using type = std::array<typename History<T, Rest...>::type, First>;
+    template<typename T, T MaxValue, std::size_t First, std::size_t... Rest>
+    struct History<T, MaxValue, First, Rest...> {
+        using type = std::array<typename History<T, MaxValue, Rest...>::type, First>;
+
+        static constexpr T maxValue = MaxValue;
     };
 
-    template<typename T, std::size_t Last>
-    struct History<T, Last> {
+    template<typename T, T MaxValue, std::size_t Last>
+    struct History<T, MaxValue, Last> {
         using type = std::array<T, Last>;
+
+        static constexpr T maxValue = MaxValue;
     };
 
     template<typename T>
-    inline void apply_gravity(T& value, const T bonus){
-        constexpr T limit = std::numeric_limits<T>::max();
+    inline void apply_gravity(T& value, const T bonus, const T limit){
         T clampedBonus = std::clamp(bonus, static_cast<T>(-limit), static_cast<T>(limit));
         value += clampedBonus - value * abs(clampedBonus) / limit;
     }
 
-    using MainHistory = History<int16_t, NUM_COLORS, NUM_SQUARES, NUM_SQUARES>::type;
+    using MainHistory = History<int16_t, std::numeric_limits<int16_t>::max(), NUM_COLORS, NUM_SQUARES, NUM_SQUARES>;
+
+    const int CONT_HIST_MAX_PLY = 1;
+    const int MAX_CONT_HIST_BONUS = 15000;
+    // prev.from(), prev.to(), curr.from(), curr.to().
+    using ContinuationHistoryEntry = History<int16_t, MAX_CONT_HIST_BONUS, NUM_SQUARES, NUM_SQUARES, NUM_SQUARES, NUM_SQUARES>;
+    using ContinuationHistory = std::array<ContinuationHistoryEntry::type, CONT_HIST_MAX_PLY>;
 
     const int TT_MOVE_VALUE = 1000000;
-    const int QUIET_OFFSET = std::numeric_limits<int16_t>::max();
+    const int QUIET_OFFSET = MainHistory::maxValue + ContinuationHistoryEntry::maxValue * CONT_HIST_MAX_PLY;
 }
 
 #endif //SIGMOID_HISTORY_HPP
--- src/worker.hpp
+++ src/worker.hpp
@@ -21,7 +21,8 @@ namespace Sigmoid {
         Timer* timer;
         int searchDepth;
 
-        MainHistory mainHistory;
+        MainHistory::type mainHistory;
+        ContinuationHistory continuationHistory;
 
         Worker(Board board, TranspositionTable* tt, WorkerHelper* wh, Timer* timer ,int searchDepth) :
             board(std::move(board)), tt(tt), workerHelper(wh), timer(timer), searchDepth(searchDepth) {}
@@ -36,14 +37,20 @@ namespace Sigmoid {
         void iterative_deepening() {
             prepare_for_search();
 
-            StackItem stack[MAX_PLY + 1];
-            StackItem* root = stack + 1;
+            StackItem stack[MAX_PLY + 2];
+            StackItem* root = stack + 2;
 
             for (int i = 0; i < MAX_PLY - 1; i++){
                 (root + i)->ply = i;
                 (root + i)->currentMove = Move::none();
                 (root + i)->excludedMove = Move::none();
             }
+
+            for (int i = 0; i < 2; i++){
+                (root - i)->currentMove = Move::none();
+                (root - i)->excludedMove = Move::none();
+            }
+
 
             for (int depth = 1; depth <= searchDepth; depth++){
                 int16_t eval = negamax<ROOT>(depth, MIN_VALUE, MAX_VALUE, root);
@@ -80,7 +87,7 @@ namespace Sigmoid {
 
             const bool in_check = board.in_check();
 
-            MoveList<false> ml(&board, &mainHistory, &entry.move);
+            MoveList<false> ml(&board, &mainHistory, &entry.move, &continuationHistory, stack);
             Move move;
             int move_count = 0;
             Move best_move = Move::none();
@@ -97,7 +104,10 @@ namespace Sigmoid {
                 move_count++;
                 result.nodesVisited++;
                 tt->prefetch(board.key());
+
+                stack->currentMove = move;
                 int16_t value = static_cast<int16_t>(-negamax<PV>(depth - 1, -beta, -alpha, stack + 1));
+                stack->currentMove = Move::none();
 
                 board.undo_move();
 
@@ -127,8 +137,11 @@ namespace Sigmoid {
                     quiet_moves.emplace_back(move);
             }
 
-            if (best_move != Move::none() && !board.is_capture(best_move))
+            if (best_move != Move::none() && !board.is_capture(best_move)){
                 update_quiet_histories(best_move, quiet_moves);
+                if (best_value >= beta)
+                    update_continuation_histories(stack, best_move, quiet_moves);
+            }
 
             if (move_count == 0 && in_check)
                 return -CHECKMATE + stack->ply;
@@ -179,10 +192,30 @@ namespace Sigmoid {
         }
 
         void update_quiet_histories(const Move& bestMove, const std::vector<Move>& quietMoves){
-            apply_gravity<int16_t>(mainHistory[board.whoPlay][bestMove.from()][bestMove.to()], 700);
+            apply_gravity<int16_t>
+                    (mainHistory[board.whoPlay][bestMove.from()][bestMove.to()], 700, MainHistory::maxValue);
 
             for (const Move& move: quietMoves)
-                apply_gravity<int16_t>(mainHistory[board.whoPlay][move.from()][move.to()], -250);
+                apply_gravity<int16_t>
+                        (mainHistory[board.whoPlay][move.from()][move.to()], -250, MainHistory::maxValue);
+        }
+
+        void update_continuation_histories(const StackItem* stack, const Move& bestMove, const std::vector<Move>& quietMoves){
+            update_continuation_histories_move(stack, bestMove, 350);
+
+            for (const Move& move : quietMoves)
+                update_continuation_histories_move(stack, move, -150);
+        }
+
+        void update_continuation_histories_move(const StackItem* stack, const Move& move, int bonus){
+            for (int n_ply = 1; n_ply <= CONT_HIST_MAX_PLY; n_ply++){
+                const Move& previous_move = (stack - n_ply)->currentMove;
+                if (previous_move == Move::none())
+                    break;
+
+                int16_t& entry = continuationHistory[n_ply - 1][previous_move.from()][previous_move.to()][move.from()][move.to()];
+                apply_gravity<int16_t>(entry, bonus, ContinuationHistoryEntry::maxValue);
+            }
         }
 
         void prepare_for_search(){
@@ -190,6 +223,13 @@ namespace Sigmoid {
                 for (auto& from : color)
                     for (auto& to: from)
                         to = 0;
+
+            for (auto& n_ply : continuationHistory)
+                for (auto& prev_from: n_ply)
+                    for (auto& prev_to: prev_from)
+                        for (auto& curr_from: prev_to)
+                            for (auto& curr_to: curr_from)
+                                curr_to = 0;
         }
     };
 }
