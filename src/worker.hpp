bebca3713b1a8cb12c7832667307b00d39b60e07
Daniel Samek <samekdaniel@email.cz>
2025-06-21 19:08:16 +0200

    iir..
    bench 4846705

--- src/worker.hpp
+++ src/worker.hpp
@@ -159,8 +159,6 @@ namespace Sigmoid {
             reset_killers(stack->ply + 1);
 
             if (!in_check && !is_singular) {
-                if ((pv_node || cutNode) && depth >= 5 && !tt_hit)
-                    depth--;
 
                 // Reverse futility pruning.
                 // If eval is really good, that even with big margin beats beta, return static eval.
@@ -188,6 +186,10 @@ namespace Sigmoid {
                     if (value >= beta)
                         return value;
                 }
+                
+                // IIR
+                if ((pv_node || cutNode) && depth >= 5 && !tt_hit)
+                    depth--;
             }
 
             MoveList<false> ml(&board, &mainHistory, &entry.move, &continuationHistory,
