4d3a899862f487283102e8a16d4fa28e5032b479
Daniel Samek <samekdaniel@email.cz>
2025-06-26 10:10:13 +0200

    32 bit TT key
    bench 4784697

--- src/tt.hpp
+++ src/tt.hpp
@@ -14,7 +14,7 @@ namespace Sigmoid {
     };
 
     struct Entry {
-        uint64_t key;
+        uint32_t key = 0;
         Move move = Move::none();
         TTFlag flag;
         int8_t depth = 0;
@@ -37,14 +37,20 @@ namespace Sigmoid {
             std::fill(entries, entries + numberOfEntries, Entry{});
         }
 
+        inline uint32_t entry_key(uint64_t key){
+            return uint32_t(key >> 32);
+        }
+
         void store(uint64_t key, const Move& move, TTFlag flag, int8_t depth, int16_t eval, int16_t ply){
             if (eval >= CHECKMATE_BOUND) eval += ply;
             else if (eval <= -CHECKMATE_BOUND) eval -= ply;
             const int index = get_index(key);
             Entry& entry = entries[index];
 
-            if (entry.key != key || depth > entry.depth || flag == EXACT)
-                entry = {key, move, flag, depth, eval};
+            uint32_t e_key = entry_key(key);
+
+            if (entry.key != e_key || depth > entry.depth || flag == EXACT)
+                entry = {e_key, move, flag, depth, eval};
         }
 
         void prefetch(uint64_t key){
@@ -55,7 +61,7 @@ namespace Sigmoid {
             const int index = get_index(key);
             Entry entry = entries[index];
 
-            const bool tt_hit = entries[index].key == key;
+            const bool tt_hit = entries[index].key == entry_key(key);
             entry.move = tt_hit ? entry.move : Move::none();
             return {entry, tt_hit};
         }
